efee2cbe53addea130c0fe42dbfd61d496cf0eab
Arthur de Paula Correa <arthur.correa@grupohpar.corp>
2023-11-16 11:30:18 -0400

    Config: Vue.js & Inertia.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/inertia-vue3";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .mount(el);
+    },
+});
